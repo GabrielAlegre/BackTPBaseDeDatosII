44884eb0a1f2e53af4ae9f21ada76f8e4639a62b
GabrielAlegre <gabrielalegre68@gmail.com>
2023-11-25 01:27:44 -0300

    porfa que ande

--- index.js
+++ index.js
@@ -1,49 +1,17 @@
-/*const express = require('express');
-const mongoose = require('mongoose');
-const cors = require('cors');
+const express = require("express");
+const cors = require("cors");
 const ticketRoutes = require('./routes/ticket');
 
 const app = express();
-
-// Middlewares
-app.use(cors());
 app.use(express.json());
 
-
-// Conectar a MongoDB Atlas
-mongoose.connect('mongodb+srv://gabrielalegre68:tpTicketera2023@tpticketera.4pcj7m3.mongodb.net/Ticketera', {
-  useNewUrlParser: true,
-  useUnifiedTopology: true
-}).then(() => console.log('MongoDB se conecto!'))
-  .catch(err => console.log('No se pudo conectar', err));
-
-// Usar rutas
-app.use('/api/tickets', ticketRoutes);
-
-const PORT = process.env.PORT || 3000;
-app.listen(PORT, () => {
-  console.log(`Server en puerto ${PORT}`);
-});*/
-
-const express = require('express');
-const mongoose = require('mongoose');
-const cors = require('cors');
-const ticketRoutes = require('./routes/ticket');
 require("dotenv").config();
-const app = express();
-
-// Middlewares
+const connect = require('./connectMongo');
+connect();
 app.use(cors());
-app.use(express.json());
-
-
-// Conectar a MongoDB Atlas
-mongoose.connect(process.env.MONGODB_URI || environment.URI).then(() => console.log('MongoDB se conecto!'))
-  .catch(err => console.log('No se pudo conectar', err));
 
 // Usar rutas
 app.use('/api/tickets', ticketRoutes);
-
 const PORT = process.env.PORT || 3000;
 app.listen(PORT, () => {
   console.log(`Server en puerto ${PORT}`);
